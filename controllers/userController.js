f77ee8cdcff08da0b4c9e548e6b1663c1cbf41dc
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-04-22 16:43:33 +0200

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This rote is not yet define',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This rote is not yet define',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This rote is not yet define',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This rote is not yet define',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    staus: 'error',
+    message: 'This rote is not yet define',
+  });
+};
---
+++ server.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    console.log('DB connection successful!', 'Env =', process.env.NODE_ENV),
+  );
+
+// const testTour = new Tour({
+//   name: 'The Park Camper',
+//   price: 997,
+// });
+
+// testTour
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => {
+//     console.log('ERROR 💥:', err);
+//   });
+
+// console.log(process.env);
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App runnihg on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
