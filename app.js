52062374d186a7c1c495f1e8de1fafea98840557
Ihar Baranau <baranovigorjsdev@gmail.com>
2024-12-02 20:20:53 +0100

    feat: updat protect routs

--- app.js
+++ app.js
@@ -10,18 +10,12 @@ const userRouter = require('./routes/userRouts');
 const app = express();
 
 // 1) MIDDLEWARES
-// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-
-// app.use((req, res, next) => {
-//   // console.log('Hello from the middleware 👋');
-//   next();
-// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -34,10 +28,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
